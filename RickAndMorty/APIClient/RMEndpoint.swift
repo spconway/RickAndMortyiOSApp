5d8037800640f2ec63ce742d7bcaeac1054aa03d
Stephen Conway <stephen.conway87@yahoo.com>
2022-12-27 17:21:49 -0500

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,18 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Stephen Conway on 12/27/22.
+//
+
+import Foundation
+
+/// Represents unique API endpoints
+enum RMEndpoint: String {
+    /// Character info
+    case character
+    /// Location info
+    case location
+    /// Episode info
+    case episode
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Stephen Conway on 12/27/22.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
